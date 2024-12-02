2394cd3682f73f84fa2ae840650930d115c65d53
Maddie Tindall <ma1130ti-s@student.lu.se>
2024-12-02 15:52:28 +0100

    chamois

--- chamois midterm.R
+++ chamois midterm.R
@@ -6,12 +6,23 @@ dat = read.table("chamois.txt", header=T)
 library(psych)
 pairs.panels(dat)
 
-#response variable: hornL+hornR = hornT
+#response variable: hornL+hornR = hornT, count, age, mass - over time
 dat$hornT <- dat$hornL+dat$hornR
 print(mean(dat$hornT))
 
 sex_count <- table(dat$sex)
 print(sex_count)
 barplot(sex_count, las = 1,
-        xlab = "Sex", ylab = "Total Hunted", ylim = c(0, 2500))
+        xlab = "Sex", ylab = "Total Chamois Hunted", ylim = c(0, 2500))
 
+season <- dat$season
+hist(season, las = 1, breaks = seq(min(year), max(year), by = 1),
+     xlim = c(1977, 2016), ylim = c(0,250),
+     xlab = "Season (year)", ylab = "Total Chamois Hunted", 
+     main = "Chamois Hunted by Season")
+
+season_count <- table(dat$season)
+print(season_count)
+barplot(season_count, las = 1, ylim = c(0,250),
+        xlab = "Season (year)", ylab = "Total Chamois Hunted", 
+        main = "Chamois Hunted by Season")
